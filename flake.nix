3dc9666230652d1111cd533ebe6d27a443df013d
Andrey Albershteyn <aalbersh@redhat.com>
2023-09-27 15:42:26 +0200

    flake fixup

--- flake.nix
+++ flake.nix
@@ -2,21 +2,22 @@
   description = "Linux Kernel development env";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     nix-kernel-vm.url = "github:alberand/nix-kernel-vm";
-    xfstests.url = "github:alberand/xfstests?rev=8eb8c15c6ee487eacdb751daee2debc83f826e6e";
+    nix-kernel-vm.inputs.nixpkgs.follows = "nixpkgs";
+    xfstests.url = "github:alberand/xfstests?branch=fsverity-v3";
     xfstests.flake = false;
     xfsprogs.url = "github:alberand/xfsprogs?rev=90ea31f2319ba2e99991545ba19d35b80c07967f";
     xfsprogs.flake = false;
-    kernel-config.url = "/home/alberand/Projects/xfs-verity-v3/.config";
+    kernel-config.url = "/home/aalbersh/Projects/kernel/xfs-verity-v3.5/.config";
     kernel-config.flake = false;
-    xfstests-config.url = "/home/alberand/Projects/xfs-verity-v3/xfstests-config";
+    xfstests-config.url = "/home/aalbersh/Projects/kernel/xfs-verity-v3.5/xfstests-config";
     xfstests-config.flake = false;
   };
 
   outputs = { self, nixpkgs, flake-utils, nix-kernel-vm, xfstests, xfsprogs,
-    kernel-config, xfstests-config}:
+    kernel-config, xfstests-config }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     system = "x86_64-linux";
@@ -38,50 +39,60 @@
           src = pkgs.fetchFromGitHub {
             owner = "alberand";
             repo = "linux";
-            rev = "3a27a5bd6d7d71f60189fcd0ba32fa91cc939fcb";
-            sha256 = "sha256-35nbE5QoVdm8S1xksmMefwtoczQGlooTHKcqxADKjUc=";
+            rev = "3edb86cbccfa4ff683dc217689e537ea1b0225ad";
+            sha256 = "sha256-N3B0Cc/EPAV+PpNC+69hN9P5XguPI6VaXoWmHh9eloE=";
           };
           configfile = kernel-config;
-          config = {
-            CONFIG_AUTOFS4_FS = "y";
-            CONFIG_VIRTIO_BLK = "y";
-            CONFIG_VIRTIO_PCI = "y";
-            CONFIG_VIRTIO_NET = "y";
-            CONFIG_EXT4_FS = "y";
-            CONFIG_NET_9P_VIRTIO = "y";
-            CONFIG_9P_FS = "y";
-            CONFIG_BLK_DEV = "y";
-            CONFIG_PCI = "y";
-            CONFIG_NETDEVICES = "y";
-            CONFIG_NET_CORE = "y";
-            CONFIG_INET = "y";
-            CONFIG_NETWORK_FILESYSTEMS = "y";
-            CONFIG_SERIAL_8250_CONSOLE = "y";
-            CONFIG_SERIAL_8250 = "y";
-            CONFIG_OVERLAY_FS = "y";
-            CONFIG_DEVTMPFS = "y";
-            CONFIG_CGROUPS = "y";
-            CONFIG_SIGNALFD = "y";
-            CONFIG_TIMERFD = "y";
-            CONFIG_EPOLL = "y";
-            CONFIG_SYSFS = "y";
-            CONFIG_PROC_FS = "y";
-            CONFIG_FHANDLE = "y";
-            CONFIG_CRYPTO_USER_API_HASH = "y";
-            CONFIG_CRYPTO_HMAC = "y";
-            CONFIG_CRYPTO_SHA256 = "y";
-            CONFIG_DMIID = "y";
-            CONFIG_TMPFS_POSIX_ACL = "y";
-            CONFIG_TMPFS_XATTR = "y";
-            CONFIG_SECCOMP = "y";
-            CONFIG_TMPFS = "y";
-            CONFIG_BLK_DEV_INITRD = "y";
-            CONFIG_MODULES = "y";
-            CONFIG_BINFMT_ELF = "y";
-            CONFIG_UNIX = "y";
-            CONFIG_INOTIFY_USER = "y";
-            CONFIG_NET = "y";
-          };
+          #config = {
+            #CONFIG_AUTOFS4_FS = "y";
+            #CONFIG_VIRTIO_BLK = "y";
+            #CONFIG_VIRTIO_PCI = "y";
+            #CONFIG_VIRTIO_NET = "y";
+            #CONFIG_VIRTIO_BALLOON = "y";
+            #CONFIG_VIRTIO_CONSOLE = "y";
+            #CONFIG_EXT4_FS = "y";
+            #CONFIG_NET_9P_VIRTIO = "y";
+            #CONFIG_9P_FS = "y";
+            #CONFIG_BLK_DEV = "y";
+            #CONFIG_PCI = "y";
+            #CONFIG_NETDEVICES = "y";
+            #CONFIG_NET_CORE = "y";
+            #CONFIG_INET = "y";
+            #CONFIG_NETWORK_FILESYSTEMS = "y";
+            #CONFIG_SERIAL_8250_CONSOLE = "y";
+            #CONFIG_SERIAL_8250 = "y";
+            #CONFIG_OVERLAY_FS = "y";
+            #CONFIG_DEVTMPFS = "y";
+            #CONFIG_CGROUPS = "y";
+            #CONFIG_SIGNALFD = "y";
+            #CONFIG_TIMERFD = "y";
+            #CONFIG_EPOLL = "y";
+            #CONFIG_SYSFS = "y";
+            #CONFIG_PROC_FS = "y";
+            #CONFIG_FHANDLE = "y";
+            #CONFIG_CRYPTO_USER_API_HASH = "y";
+            #CONFIG_CRYPTO_HMAC = "y";
+            #CONFIG_CRYPTO_SHA256 = "y";
+            #CONFIG_DMIID = "y";
+            #CONFIG_TMPFS_POSIX_ACL = "y";
+            #CONFIG_TMPFS_XATTR = "y";
+            #CONFIG_SECCOMP = "y";
+            #CONFIG_TMPFS = "y";
+            #CONFIG_BLK_DEV_INITRD = "y";
+            #CONFIG_MODULES = "y";
+            #CONFIG_BINFMT_ELF = "y";
+            #CONFIG_UNIX = "y";
+            #CONFIG_INOTIFY_USER = "y";
+            #CONFIG_NET = "y";
+            ## SCSI
+            #CONFIG_SCSI = "y";
+            #CONFIG_DMA = "y";
+            #CONFIG_SG_POOL = "y";
+            #CONFIG_SCSI_COMMON = "y";
+            #CONFIG_BLK_DEV_BSG_COMMON  = "y";
+            #CONFIG_SCSI_LOWLEVEL = "y";
+            #CONFIG_SCSI_DEBUG = "y";
+          #};
         }));
 
         # Let's enable xfstests service
@@ -91,7 +102,9 @@
           sharedir = "/root/vmtest";
           autoshutdown = false;
           testconfig = xfstests-config;
-          arguments = "-g auto";
+          test-dev = "/dev/sda";
+          scratch-dev = "/dev/sdb";
+          arguments = "-r -g all -x recoveryloop,dangerous_repair,dangerous_bothrepair,dangerous_online_repair,dangerous_norepair,broken,deprecated,dangerous_fuzzers,dangerous_scrub";
         };
 
         # Let's also include specific version of sprogs
@@ -117,7 +130,8 @@
       inherit pkgs root;
 
       qemu-options = [
-        "-hdc /dev/sdb4 -hdd /dev/sdb5 -serial mon:stdio"
+        "-hdb /dev/loop0"
+        "-hdc /dev/loop1"
       ];
 
       user-modules = modules;
