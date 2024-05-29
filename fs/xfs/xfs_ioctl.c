173e323765a169c7d40af83ec15038bc354e66b4
Andrey Albershteyn <aalbersh@kernel.org>
2024-05-28 21:15:29 -0700

    xfs: add fs-verity ioctls
    Add fs-verity ioctls to enable, dump metadata (descriptor and Merkle
    tree pages) and obtain file's digest.
    
    [djwong: remove unnecessary casting]
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Andrey Albershteyn <aalbersh@kernel.org>

--- fs/xfs/xfs_ioctl.c
+++ fs/xfs/xfs_ioctl.c
@@ -44,6 +44,7 @@
 
 #include <linux/mount.h>
 #include <linux/fileattr.h>
+#include <linux/fsverity.h>
 
 /* Return 0 on success or positive error */
 int
@@ -1418,6 +1419,21 @@ xfs_file_ioctl(
 		return xfs_ioc_start_commit(filp, arg);
 	case XFS_IOC_COMMIT_RANGE:
 		return xfs_ioc_commit_range(filp, arg);
+
+	case FS_IOC_ENABLE_VERITY:
+		if (!xfs_has_verity(mp))
+			return -EOPNOTSUPP;
+		return fsverity_ioctl_enable(filp, arg);
+
+	case FS_IOC_MEASURE_VERITY:
+		if (!xfs_has_verity(mp))
+			return -EOPNOTSUPP;
+		return fsverity_ioctl_measure(filp, arg);
+
+	case FS_IOC_READ_VERITY_METADATA:
+		if (!xfs_has_verity(mp))
+			return -EOPNOTSUPP;
+		return fsverity_ioctl_read_metadata(filp, arg);
 
 	default:
 		return -ENOTTY;
