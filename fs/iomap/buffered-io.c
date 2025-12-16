976335064a7ac0452dbad606da2d23b099e3f6f2
Christoph Hellwig <hch@lst.de>
2025-12-16 07:54:29 +0100

    iomap: only call into ->submit_read when there is a read_ctx
    Move the NULL check into the callers to simplify the callees.
    
    Fuse was missing this before, but has a constant read_ctx that is
    never NULL or changed, so no change here either.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
    Tested-by: Anuj Gupta <anuj20.g@samsung.com>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -572,7 +572,7 @@ void iomap_read_folio(const struct iomap_ops *ops,
 		iter.status = iomap_read_folio_iter(&iter, ctx,
 				&bytes_submitted);
 
-	if (ctx->ops->submit_read)
+	if (ctx->read_ctx && ctx->ops->submit_read)
 		ctx->ops->submit_read(&iter, ctx);
 
 	iomap_read_end(folio, bytes_submitted);
@@ -636,7 +636,7 @@ void iomap_readahead(const struct iomap_ops *ops,
 		iter.status = iomap_readahead_iter(&iter, ctx,
 					&cur_bytes_submitted);
 
-	if (ctx->ops->submit_read)
+	if (ctx->read_ctx && ctx->ops->submit_read)
 		ctx->ops->submit_read(&iter, ctx);
 
 	if (ctx->cur_folio)
